c02e8c618a3aeb0b79bd3203d1ba77be08be5c5c
Lauren Hollis <leholllis@gmail.com>
2019-07-12 11:07:23 -0500

    Completed second iteration

--- ViewController.swift
+++ ViewController.swift
@@ -15,14 +15,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    let emojis = ["🏊‍♀️": "swimming", "🚣‍♀️": "rowing"]
+    let emojis = ["🏊‍♀️": "Swimming", "🚣‍♀️": "Rowing", "🏃‍♀️" : "Running","⛹️‍♀️":"Basketball"]
+    var facts = ["Swimming" : "yum water", "Rowing" : "ew water","Running":"yas shin splints", "Basketball":"Troy Bolton"]
     
     @IBAction func showMessage(sender:UIButton){
         //if user clicks button, show alert that has something to do with that sport
         let selectedSport = sender.titleLabel?.text
+        let emojiFact = facts[emojis[selectedSport!]!]
         
-        let alertController = UIAlertController(title: "This is an alert", message: "Hello!", preferredStyle: UIAlertController.Style.alert)
+        let alertController = UIAlertController(title: emojis[selectedSport!], message: emojiFact, preferredStyle: UIAlertController.Style.alert)
+        
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+
+        alertController.addAction(UIAlertAction(title: "BYE", style: UIAlertAction.Style.default, handler: nil))
+        
         present(alertController, animated: true, completion: nil)
         
     }
